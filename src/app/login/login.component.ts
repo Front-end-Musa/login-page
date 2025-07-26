05d417b2d59f788a292f251e540878aaf99dc816
Rufatuly Musa <rufatulymusa567@gmail.com>
2025-07-26 17:28:02 +0100

    basics

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Here you would typically handle the login logic, e.g., call an authentication service
+      console.log('Login successful', this.loginForm.value);
+      this.router.navigate(['/home']); // Navigate to home on successful login
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
